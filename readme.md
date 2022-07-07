0da52029027deae02b0d79a893e150faa3ec6264
NicoLorenzo1 <88661584+NicoLorenzo1@users.noreply.github.com>
2022-07-07 20:57:48 -0300

    Entrega finalizada

--- readme.md
+++ readme.md
@@ -2,9 +2,24 @@
 
 ![img](https://images-na.ssl-images-amazon.com/images/I/A13vvsoih5L.png)
 
-### El diagrama UML del proyecto y el informe de nuestra solución se encuentra cargado en "docs".
+### Defensa final del proyecto.
 
-Rutas de archivos necesarios:
-src/Program/bin/Debug/net6.0/secrets.json
+Ruta de archivo necesario en caso de que de problema con las carpeta appsettings.json
 src/Program/bin/Debug/net6.0/appsettings.json
+pegar esto dentro :
 
+{
+  "BotSecret": {
+      "Token": "Set in Azure. For development, set in User Secrets"
+  }
+}
+
+Ruta de archivo necesario en caso de que de problema con las carpeta secrets.json
+
+src/Program/bin/Debug/net6.0/secrets.json
+
+pegar dentro:
+
+{
+  "BotSecret:Token": "5407703062:AAFxEi-d8D-mns_Lj6cTW6mJB4tX1naVXo8"
+}
--- src/Library/BotRelatedV2/CounterShotsHandler.cs
+++ src/Library/BotRelatedV2/CounterShotsHandler.cs
@@ -3,11 +3,14 @@ using Telegram.Bot.Types;
 namespace Library
 {
     /// <summary>
-    /// Clase CounterShotsHandler que se encarga de enviarle al usuario la cantidad de tiros que tocaron agua, los que tocaron barcos, y los totales.
+    /// Clase CounterShotsHandler que se encarga de enviarle al usuario la cantidad de tiros que tocaron agua, los que tocaron barcos, y los disparos totales.
     /// Saca la información de game que es quien conoce la cantidad de shots.
+    /// Para acceder a el debemos ejecutar el comando "disparos" o "Disparos" y nos mostrara la información que necesitamos.
     /// </summary>
     public class CounterShotsHandler : BaseHandler
     {
+        private int resutlTotalSipShots = 0;
+        private int result = 0;
         public CounterShotsState State { get; set; }
         public CounterShotsHandler(BaseHandler next) : base(next)
         {
@@ -24,22 +27,20 @@ namespace Library
             response = string.Empty;
             if (State == CounterShotsState.ShowShots)
             {
-
                 Game game = Administrator.Instance.GetPlayerGame(message.From.Id);
 
-                int resutlTotalSipShots = game.hitsPlayer1 + game.hitsPlayer2;
+                resutlTotalSipShots = game.hitsPlayer1 + game.hitsPlayer2;
 
-                int result = game.watherShots + resutlTotalSipShots;
+                result = game.watherShots + resutlTotalSipShots;
 
                 if (game.player1.Id == message.From.Id)
                 {
-                    Bot.sendTelegramMessage(game.player1, $"Los disparos que tocaron agua fueron: {game.watherShots}\n Los disparos que tocaron barco fueron: {resutlTotalSipShots} \n Los disparos totales fueron {result}");
+                    response = $"Los disparos que tocaron agua fueron: {game.watherShots}\n Los disparos que tocaron barco fueron: {resutlTotalSipShots} \n Los disparos totales fueron {result}";
                 }
                 else
                 {
-                    Bot.sendTelegramMessage(game.player2, $"Los disparos que tocaron agua fueron: {game.watherShots}\n Los disparos que tocaron barco fueron: {resutlTotalSipShots} \n Los disparos totales fueron {result}");
+                    response = $"Los disparos que tocaron agua fueron: {game.watherShots}\n Los disparos que tocaron barco fueron: {resutlTotalSipShots} \n Los disparos totales fueron {result}";
                 }
-
             }
         }
         public enum CounterShotsState
